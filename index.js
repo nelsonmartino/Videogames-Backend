4d14a42074d9ad7b1e5a5766436b32034814722e
nelsonmartino <nelsonmartino@gmail.com>
2023-11-17 00:02:12 -0300

    deploy mods 1

--- index.js
+++ index.js
@@ -19,7 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = 3001;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
